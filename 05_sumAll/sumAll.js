f13608e72e2014d941046f8bd66b763cc896fea4
Rami Hsasna <ramihsasna34@gmail.com>
2024-06-09 15:32:16 +0100

    Proposed solution for sumAll and passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (number1, number2) {
+  let sum = 0;
+  if (number1 < 0 || number2 < 0) return "ERROR";
+  if (isNaN(number1) || isNaN(number2)) return "ERROR";
+  if (number1 < number2) {
+    for (let i = number1; i <= number2; i++) sum += i;
+  } else {
+    for (let i = number2; i <= number1; i++) sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
